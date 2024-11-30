e7943b2ded7d4b9bd34b45124c7892f3407ee596
dar2006sh8an13 <darshanpl1382006@gmail.com>
2024-11-30 18:35:30 +0530

    Add files via upload
    today code using c program to print the # in different type

--- darshan6.c
+++ darshan6.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-int main(){
-    printf("Hello, World!\n");
-return 0;
+void main()
+{
+  int i,j;
+  for(i=1;i<=5;i++)
+{
+  for(j=1;j<=j;j++)
+{
+printf("#");
+}
+printf("\n");
 }
